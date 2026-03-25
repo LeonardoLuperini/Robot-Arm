9399c4c93ee1ebaa363fea7981b6fc5cbcfdf8d2
LeonardoLuperini <leonardo.luperini.29@gmail.com>
2026-03-25 16:46:15 +0100

    Now framebuffer_size_callback works as intended

--- src/main.zig
+++ src/main.zig
@@ -8,9 +8,9 @@ const Shader = @import("shader.zig");
 const Cube = shapes.Cube;
 const RenCube = shapes.RenderableCube;
 
-const gl = zopengl.bindings;
-const uint = gl.Uint;
-const int = gl.Int;
+const gl   = zopengl.bindings;
+const uint = gl.Uint; // c_uint
+const int  = gl.Int;  // equal to c_int
 const Mat3 = za.Mat3;
 const Mat4 = za.Mat4;
 const Vec3 = za.Vec3;
@@ -18,9 +18,17 @@ const Vec4 = za.Vec4;
 
 const gl_version_major = 4;
 const gl_version_minor = 1;
-const sugg_width: comptime_float = 1920;
-const sugg_height: comptime_float = 1080;
-const aspect: comptime_float = sugg_width/sugg_height;
+const default_width: comptime_float = 1920;
+const default_height: comptime_float = 1080;
+const default_aspect: comptime_float = default_width/default_height;
+const fovy: comptime_int = 60;
+const z_near: comptime_int = 10;
+const z_far: comptime_int = 50;
+
+const RenderState = struct {
+    shader_program: uint,
+    uProj: int,
+};
 
 pub fn main() !void {
     try glfw.init();
@@ -34,7 +42,7 @@ pub fn main() !void {
     glfw.windowHint(.doublebuffer, true);
     glfw.windowHint(.resizable, true);
 
-    const window = try glfw.createWindow(sugg_width, sugg_height, "Hello World", null, null);
+    const window = try glfw.createWindow(default_width, default_height, "Hello World", null, null);
     defer window.destroy();
 
     glfw.makeContextCurrent(window);
@@ -57,16 +65,23 @@ pub fn main() !void {
 
     var rc = RenCube.new(cube);
 
-    const projection = za.perspective(60, aspect, 10, 50);
+    // Perspective Matrix
+    const projection = za.perspective(fovy, default_aspect, z_near, z_far);
     const uProj = gl.getUniformLocation(shader.program, "uProj");
     gl.uniformMatrix4fv(uProj, 1, gl.FALSE, &projection.data[0]);
+
+    var render_state = RenderState{
+        .shader_program = shader.program,
+        .uProj = uProj,
+    };
+
+    window.setUserPointer(@ptrCast(&render_state));
 
     gl.clearColor(0.2, 0.2, 0.2, 1);
     while (!glfw.windowShouldClose(window)) {
 
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        // gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, null);
         rc.draw();
 
         window.swapBuffers();
@@ -76,8 +91,16 @@ pub fn main() !void {
 
 }
 
-fn framebuffer_size_callback(window: *glfw.Window, fb_w: c_int, fb_h: c_int) callconv(.c) void {
-    _ = window;
-    _ = fb_w;
-    _ = fb_h;
+fn framebuffer_size_callback(window: *glfw.Window, fb_w: int, fb_h: int) callconv(.c) void {
+    gl.viewport(0, 0, fb_w, fb_h);
+
+    if (fb_h == 0) return; // Protects from division by 0 in case the window is minimmized
+
+    const aspect: f32 = @as(f32, @floatFromInt(fb_w)) / @as(f32, @floatFromInt(fb_h));
+    const projection = za.perspective(fovy, aspect, z_near, z_far);
+
+    const rs: RenderState = window.getUserPointer(RenderState).?.*;
+
+    gl.useProgram(rs.shader_program);
+    gl.uniformMatrix4fv(rs.uProj, 1, gl.FALSE, &projection.data[0]);
 }
